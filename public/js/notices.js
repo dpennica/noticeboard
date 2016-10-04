dd4d7e565936095d703742aa713d603ca4c511ee
Davide Pennica <dpennica73@yahoo.it>
2016-10-04 19:45:21 +0200

    modifica style

--- public/js/notices.js
+++ public/js/notices.js
@@ -4,5 +4,7 @@ function show_form(form_id){
   $("form").hide();
 
   $('#'+form_id).show("slow");
+  $('#add-postit').hide("slow");
+
 
 }
--- resources/views/notices.blade.php
+++ resources/views/notices.blade.php
@@ -4,63 +4,59 @@
 
 <section id="form_section">
     <!-- add section -->
-    <form id="add_notice" class="notice col-md-8" style="display:none" >
-        <div class="form-group">
-            <label for="exampleInputEmail1">Titolo</label>
-            <input id="message_title" type="text" name="title"placeholder="Titolo del messaggio" value="" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Messaggio</label>
-            <input id="message_description" type="text" name="description"placeholder="Inserisci il messaggio" value="" class="form-control">
-        </div>
-        <button type="button" class="btn btn-default">Aggiungi messaggio</button>
-    </form>
-    <!-- edit section -->
-    <form id="edit_notice" class="notice" style="display:none">
-        <input id="edit_message_id" type="hidden" value="" />
-        <input id="edit_message_title" type="text"name="title" value="" />
-        <button name="submit">Edit Task</button>
-    </form>
-
+    <div class="row" >
+        <form id="add_notice" class="notice col-ms-8 col-md-8" style="display:none" >
+            <div class="form-group">
+                <label for="message_title">Titolo</label>
+                <input class="form-control" id="message_title" type="text" name="title" placeholder="Titolo del messaggio" value="" >
+            </div>
+            <div class="form-group">
+                <label for="message_description">Messaggio</label>
+                <textarea class="form-control" id="message_description" name="description" placeholder="Inserisci il messaggio" rows="5" ></textarea>
+            </div>
+            <button type="button" class="btn btn-default">Aggiungi PostIt</button>
+        </form>
+        <!-- edit section -->
+        <form id="edit_notice" class="notice" style="display:none">
+            <input id="edit_message_id" type="hidden" value="" />
+            <input id="edit_message_title" type="text"name="title" value="" />
+            <button name="submit">Edit Task</button>
+        </form>
+    </div>
 </section>
 
 <!-- show section -->
-<section id="data_section" class="notice">
-    <ul class="todo-controls">
-        <li><img src="imgs/add.png" width="14px" onClick="show_form('add_notice');" /></li>
-    </ul>
-    <ul id="message_list" class="notice-list">
-        @foreach($notices as $notice)
-        @if(!$notice->status)
-        <div class="postit done">
-            <li id="{{$notice->id}}" >
-                <a href="#" class="toggle"></a>
-                <span id="span_{{$notice->id}}" class="notice-title">{{$notice->title}}</span><br>
-                <span class="notice-description">{{$notice->description}}</span>
-                <a href="#"onClick="delete_message('{{$notice->id}}');"class="icon-delete">Delete</a>
-                <a href="#"onClick="edit_message('{{$notice->id}}','{{$notice->title}}');"class="icon-edit">Edit</a>
-            </li>
-        </div>
-        @else
-        <div class="postit">
-            <li id="{{$notice->id}}">
-                <a href="#"onClick="message_done('{{$notice->id}}');"class="toggle"></a>
-                <span id="span_{{$notice->id}}" class="notice-title">{{$notice->title}}</span><br>
-                <span class="notice-description">{{$notice->description}}</span>
-                <a href="#" onClick="delete_message('{{$notice->id}}');" class="icon-delete">Delete</a>
-                <a href="#" onClick="edit_message('{{$notice->id}}','{{$notice->title}}');"class="icon-edit">Edit</a>
-            </li>
-        </div>
-        @endif
+<section id="data_section" >
 
-        @endforeach
-    </ul>
+    <div class="row notice col-md-8 col-lg-12" >
+        <p>
+        <button type="button" id="add-postit" class="btn btn-primary" onClick="show_form('add_notice');">Aggiungi PostIt</button>
+        </p>
 
+        <ul class="postit" >
+            @foreach($notices as $notice)
+                @if(!$notice->status)
+                <li >
+                    <i class="pin"></i>
+                    <div id="{{$notice->id}}" class="note yellow">
+                        <small>{{$notice->created_at}}</small>
+                        <h3><span id="span_{{$notice->id}}" class="notice-title">{{$notice->title}}</span></h3>
+                        <p>
+                            <span class="notice-description">{{$notice->description}}</span>
+                        </p>
+                        <a href="#"onClick="delete_message('{{$notice->id}}');"class="icon-delete">Delete</a>
+                        <a href="#"onClick="edit_message('{{$notice->id}}','{{$notice->title}}');"class="icon-edit">Edit</a>
+                    </div>
+                </li>
+                @endif
+            @endforeach
+        </ul>
+    </div>
 </section>
 
 @endsection
 
 @section('footer')
-<script src="http://code.jquery.com/jquery-latest.min.js"type="text/javascript"></script>
+<script src="js/jquery-3.1.1.min.js"type="text/javascript"></script>
 <script src="js/notices.js"type="text/javascript"></script>
 @endsection
